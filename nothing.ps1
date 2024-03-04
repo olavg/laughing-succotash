85fb64834bda53f1f963a0dfd25943d8a8907ac4
Olav Garborg <olav.garborg@nov.com>
2024-03-04 13:19:00 +0100

    Update nothing.ps1 - add reverse move if close to edge

--- nothing.ps1
+++ nothing.ps1
@@ -5,26 +5,45 @@ Add-Type @"
     public class MouseEvents {
         [DllImport("user32.dll")]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT {
+            public int X;
+            public int Y;
+        }
 
         public const int MOUSEEVENTF_MOVE = 0x0001;
     }
 "@
-# Define the exit time (5 PM on the current day)
-$exitTime = Get-Date -Hour 17 -Minute 0 -Second 0
-do {
-    # Check the current time
-    $currentTime = Get-Date
 
-    # Exit the script if current time is equal to or later than 5 PM
-    if ($currentTime -ge $exitTime) {
-        break
+do {
+    # Get the current cursor position
+    $point = New-Object MouseEvents.POINT
+    [MouseEvents]::GetCursorPos([ref]$point)
+    $xMove = 1  # Default movement to the right
+
+    # If the cursor is too close to the left edge of the screen, move right instead
+    if ($point.X -le 0) {
+        $xMove = 1
+    } else {
+        $xMove = -1  # Move left if not close to the edge
     }
 
-    # Move mouse cursor one pixel to the left
-    [MouseEvents]::mouse_event([MouseEvents]::MOUSEEVENTF_MOVE, -1, 0, 0, 0)
+    # Move mouse cursor based on $xMove direction
+    [MouseEvents]::mouse_event([MouseEvents]::MOUSEEVENTF_MOVE, $xMove, 0, 0, 0)
     Start-Sleep -Milliseconds 100
-    # Move mouse cursor one pixel to the right
-    [MouseEvents]::mouse_event([MouseEvents]::MOUSEEVENTF_MOVE, 1, 0, 0, 0)
+
+    # Move mouse cursor back to the original position
+    [MouseEvents]::mouse_event([MouseEvents]::MOUSEEVENTF_MOVE, $xMove * -1, 0, 0, 0)
+
     # Wait for one minute
     Start-Sleep -Seconds 60
+
+    # Check if the current time is 5 PM or later
+    $currentTime = Get-Date
+    if ($currentTime.Hour -ge 17) {
+        break  # Exit the loop
+    }
 } while ($true)
