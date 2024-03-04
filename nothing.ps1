79902497d58074edbaa0953b36b999909972f232
Olav Garborg <olav.garborg@nov.com>
2024-03-04 13:16:36 +0100

    Update nothing.ps1 - exit at 5PM

--- nothing.ps1
+++ nothing.ps1
@@ -9,8 +9,17 @@ Add-Type @"
         public const int MOUSEEVENTF_MOVE = 0x0001;
     }
 "@
-
+# Define the exit time (5 PM on the current day)
+$exitTime = Get-Date -Hour 17 -Minute 0 -Second 0
 do {
+    # Check the current time
+    $currentTime = Get-Date
+
+    # Exit the script if current time is equal to or later than 5 PM
+    if ($currentTime -ge $exitTime) {
+        break
+    }
+
     # Move mouse cursor one pixel to the left
     [MouseEvents]::mouse_event([MouseEvents]::MOUSEEVENTF_MOVE, -1, 0, 0, 0)
     Start-Sleep -Milliseconds 100
